2fd1f36c2013eb2aaf79437d7ee1c2b8408a2c2a
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-25 19:52:45 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/cadastrandocontatosunicos/CadastrandoContatosUnicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrandocontatosunicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrandoContatosUnicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastrandoContatosUnicosApplication.class, args);
+    }
+
+}
